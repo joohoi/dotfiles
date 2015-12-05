cdca38440544edfcac0dd06ff81b6a7aa6405daf
Joona Hoikkala <joona@kuori.org>
2015-12-05 16:44:55 +0200

    Added sane configuration to git prompt

--- bash/quick-install.sh
+++ bash/quick-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/magicmonty/bash-git-prompt ~/.bash_git_prompt
+echo -e "GIT_PROMPT_ONLY_IN_REPO=0\nGIT_PROMPT_FETCH_REMOTE_STATUS=0" >> ~/.profile
 echo "source ~/.bash_git_prompt" >> ~/.profile
