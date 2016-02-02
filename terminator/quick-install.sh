5c69ead52e7541dfd981322288fe66bb1cff2f4c
Joona Hoikkala <joona@kuori.org>
2016-02-02 14:37:15 +0200

    Fixed solarized

--- terminator/quick-install.sh
+++ terminator/quick-install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 mkdir -p ~/.config/terminator
 cp terminator ~/.config/terminator/config
-git clone https://github.com/ghuntley/terminator-solarized.git
-cd terminator-solarized
-cat config >> ~/.config/terminator/config
