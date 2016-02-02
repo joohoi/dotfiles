edb43783f1e190548325c231b9bc6688903be62c
Joona Hoikkala <joona@kuori.org>
2016-02-02 14:32:22 +0200

    Added solarized theme to terminator config

--- terminator/quick-install.sh
+++ terminator/quick-install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.config/terminator
 cp terminator ~/.config/terminator/config
+git clone https://github.com/ghuntley/terminator-solarized.git
+cd terminator-solarized
+cat config >> ~/.config/terminator/config
