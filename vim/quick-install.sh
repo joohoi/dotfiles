d457d86f1b60b1de8fe6d0e85e34bf5a873b50b1
Joona Hoikkala <joona@kuori.org>
2018-04-07 10:07:56 +0300

    Finetuning Xresources and vim install

--- vim/quick-install.sh
+++ vim/quick-install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # vimrc
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/plugged
 cp ./vimrc ~/.vimrc
 
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p ~/.vim/plugged
